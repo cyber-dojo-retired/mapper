2b4cc2ee145fa8812afae969e7b88f94c0220a26
Jon Jagger <jon@jaggersoft.com>
2019-06-01 20:48:15 +0100

    tidy ci script

--- sh/travis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ev
-
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  ./pipe_build_up_test.sh
-  echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-  docker push cyberdojo/mapper
-fi
